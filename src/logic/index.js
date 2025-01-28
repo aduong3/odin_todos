26b49b6391166a6defd6ce7f3998c08f0c9e5d1d
aduong3 <mythalink@gmail.com>
2025-01-28 02:13:43 -0800

    manipulating DOM, styling sidebar first to set up navigations

--- src/logic/index.js
+++ src/logic/index.js
@@ -1,30 +1,8 @@
 import "../styles/index.styles.css";
-// import sidebar from './sidebar.js';
+import sidebar from './sidebar.js';
 
 import { Projects } from './projects.js';
 import { projectManager } from "./projectManager.js";
 
-const body = document.querySelector("body");
-
-const form = document.createElement("form");
-const title = document.createElement("input");
-title.setAttribute("type", "text");
-title.setAttribute("id", "title");
-title.setAttribute("name", "title");
-form.appendChild(title);
-
-const submitButton = document.createElement("button");
-submitButton.setAttribute("type", "submit");
-submitButton.textContent = "Submit";
-form.appendChild(submitButton);
-body.appendChild(form);
-
-
-const display = document.createElement("p");
-display.classList.add("display");
-
-body.appendChild(display);
-
-window.Projects = Projects;
-window.projectManager = projectManager;
+sidebar();
 
--- src/logic/sidebar.js
+++ src/logic/sidebar.js
@@ -2,4 +2,39 @@ import '../styles/sidebar.styles.css';
 
 export default function sidebar(){
     
+    const sidebarContainer = document.createElement('div');
+    sidebarContainer.classList.add('sidebar');
+
+
+
+//-----------------------------------------------------GET TASKS SORTED BY DATE-------------------------------------------------
+
+    const sortByDiv = document.createElement('div');
+    sortByDiv.classList.add('sortByDiv');
+
+    const fieldset = document.createElement('fieldset');
+    fieldset.setAttribute('role', 'presentation');
+
+    const legend = document.createElement('legend');
+    legend.textContent = 'Sort By';
+
+    const today = document.createElement('p');
+    today.textContent = 'today';
+    today.classList.add('today');
+
+    fieldset.appendChild(legend);
+    fieldset.appendChild(today);
+
+    sortByDiv.appendChild(fieldset);
+
+
+    sidebarContainer.appendChild(sortByDiv);
+    document.body.appendChild(sidebarContainer);
+
 }
+
+
+
+
+
+
