9066bd084fcb7549704961fdb67b8a4908b5e768
aduong3 <mythalink@gmail.com>
2025-02-02 23:33:04 -0800

    removed the menu icon from today and upcoming, no need to have any ability to edit those two

--- src/logic/sidebar.js
+++ src/logic/sidebar.js
@@ -204,10 +204,6 @@ export default function sidebar() {
 
   //<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>dots-horizontal</title><path d="M16,12A2,2 0 0,1 18,10A2,2 0 0,1 20,12A2,2 0 0,1 18,14A2,2 0 0,1 16,12M10,12A2,2 0 0,1 12,10A2,2 0 0,1 14,12A2,2 0 0,1 12,14A2,2 0 0,1 10,12M4,12A2,2 0 0,1 6,10A2,2 0 0,1 8,12A2,2 0 0,1 6,14A2,2 0 0,1 4,12Z" /></svg>
 
-  const todayMenuIcon = createMenuIcon();
-  const upcomingMenuIcon = createMenuIcon();
-  todayDiv.appendChild(todayMenuIcon);
-  upcomingDiv.appendChild(upcomingMenuIcon);
 
   sortByDiv.appendChild(todayDiv);
   sortByDiv.appendChild(upcomingDiv);
